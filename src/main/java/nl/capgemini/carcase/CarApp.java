a853730ddbc79b5561275216d942aabef6eaee63
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-12-19 08:29:08 +0100

    Finish exercise 3

--- src/main/java/nl/capgemini/carcase/CarApp.java
+++ src/main/java/nl/capgemini/carcase/CarApp.java
@@ -1,11 +1,48 @@
 package nl.capgemini.carcase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarApp {
    public static void main(String[] args) {
       System.out.println("Welcome to my very first car application! Yahoo!");
-      
+
+      // exercise 2
       int speed = 80;
-      
+
       System.out.println(speed);
+
+      // exercise 3
+      int age = 45;
+      int yearOfRegistration = 1998;
+
+      int[] numbers = new int[] { speed, age, yearOfRegistration };
+
+      System.out.println(numbers);
+
+      // print in loop
+      for (int number : numbers) {
+         System.out.println(number);
+      }
+
+      // alternate to assign the array
+      int[] alternateNumbers = { speed, age, yearOfRegistration };
+      System.out.println(alternateNumbers);
+
+      // exercise 3 extra
+      List<Integer> numberList = new ArrayList<>();
+
+      numberList.add(speed);
+      numberList.add(age);
+      numberList.add(yearOfRegistration);
+
+      for (int num : numberList) {
+         System.out.println(num);
+      }
+
+      // alternate / stream
+      numberList.stream().forEach(num -> {
+         System.out.println(num);
+      });
    }
 }
