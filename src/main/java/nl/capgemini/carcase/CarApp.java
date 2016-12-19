962cbf709ff3c1fb20516c682eec44672b3c317d
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-12-19 08:26:01 +0100

    Finish exercise 1

---
+++ src/main/java/nl/capgemini/carcase/CarApp.java
@@ -0,0 +1,7 @@
+package nl.capgemini.carcase;
+
+public class CarApp {
+   public static void main(String[] args) {
+      System.out.println("Welcome to my very first car application! Yahoo!");
+   }
+}
