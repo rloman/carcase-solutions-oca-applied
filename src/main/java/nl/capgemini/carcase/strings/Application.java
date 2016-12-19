58aa1547b2424e1b19ceef33c7ca3d90ebf816e5
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-12-19 14:31:02 +0100

    Finish exercise 11

--- src/main/java/nl/capgemini/carcase/strings/Application.java
+++ src/main/java/nl/capgemini/carcase/strings/Application.java
@@ -10,6 +10,30 @@ public class Application {
 
       // rest of this part of exercise omitted
 
+      // exercise 11
+      String c = "Compute";
+      String d = "r";
+
+      String result = "Computer";
+
+      String concat = c + d;
+
+      System.out.println(concat);
+
+      System.out.println(concat.equals(result));
+
+      StringBuilder b = new StringBuilder();
+
+      // ASSIGNMENT !!!!!!!!!!!
+      // create the String "Computer" using this StringBuilder instance
+      b.append("Computer");
+
+      String computer = b.toString();
+
+      System.out.println(computer.equals("Computer"));
+      // SHOULD BE true!!!
+
+      System.out.println(computer == result); // bad practise but test for now
    }
 
 }
