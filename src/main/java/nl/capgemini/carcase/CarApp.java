80f3d2cd508e8ad1bc1bd051804afc4977e2c65a
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-12-19 08:27:23 +0100

    Finish exercise 2

--- src/main/java/nl/capgemini/carcase/CarApp.java
+++ src/main/java/nl/capgemini/carcase/CarApp.java
@@ -3,5 +3,9 @@ package nl.capgemini.carcase;
 public class CarApp {
    public static void main(String[] args) {
       System.out.println("Welcome to my very first car application! Yahoo!");
+      
+      int speed = 80;
+      
+      System.out.println(speed);
    }
 }
