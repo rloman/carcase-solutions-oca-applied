0a78902b90fdc2caa2a91ea4369c8c89be0fbc1b
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-12-22 10:52:26 +0100

    Add Utils for demo

---
+++ src/main/java/nl/capgemini/carcase/Utils.java
@@ -0,0 +1,15 @@
+package nl.capgemini.carcase;
+
+public class Utils {
+
+   public Utils() {
+
+   }
+
+   public static int max(int... getallen) {
+      int max = getallen[0];
+
+      return max;
+   }
+
+}
