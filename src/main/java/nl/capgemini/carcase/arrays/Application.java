f6d1aa1d6df0697b15f17cc2bb4649d9b0e54c42
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-12-22 16:57:42 +0100

    some nice training for arrays with tables

---
+++ src/main/java/nl/capgemini/carcase/arrays/Application.java
@@ -0,0 +1,39 @@
+package nl.capgemini.carcase.arrays;
+
+public class Application {
+
+   public static void main(String[] args) {
+
+      // tables
+
+      int[][] tables = new int[11][11];
+
+      for (int i = 1; i < 11; i++) {
+         for (int j = 1; j < 11; j++) {
+            tables[i][j] = i * j;
+         }
+      }
+
+      for (int i = 1; i < 11; i++) {
+         System.out.print("Table of:" + i + "\t");
+      }
+      System.out.println();
+      int tabCounter = 0;
+      for (int i = 1; i < 11; i++) {
+         String tabString = "";
+         for (int tabs = 0; tabs < tabCounter; tabs++) {
+            tabString += "\t";
+         }
+         // System.out.print(tabString);
+         tabCounter++;
+         for (int j = 1; j < 11; j++) {
+
+            String result = String.format("%02dx%02d=%03d ", i, j, tables[i][j]);
+            System.out.print(result + "\t");
+         }
+         System.out.println();
+      }
+
+   }
+
+}
